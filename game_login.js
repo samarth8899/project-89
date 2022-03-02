de45ecb3dd18e31291bf3037ac510536ad677b5f
samarth8899 <95847443+samarth8899@users.noreply.github.com>
2022-03-02 15:48:36 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,9 @@
+function login()
+{
+    player1=document.getElementById("player1_name_input").value;
+    localStorage.setItem("player1_name",player1);
+
+    player2=document.getElementById("player2_name_input").value;
+    localStorage.setItem("player2_name",player2);
+    window.location="game_page.html"
+}
